67d1529cac300e386956f51b118d8b85063f5bad
Leul Degarege <leuldegarege11@gmail.com>
2023-08-03 10:48:29 +0300

    shapes done

--- Program.cs
+++ Program.cs
@@ -3,24 +3,36 @@
     static void Main(string[] args){
 
         //checking the palindrome function
-        Palindrome palindrome = new Palindrome();
-        Console.WriteLine(palindrome.isPalindrome("racecar"));
-        Console.WriteLine(palindrome.isPalindrome("hello"));
-        Console.WriteLine(palindrome.isPalindrome("madam"));
+        // Palindrome palindrome = new Palindrome();
+        // Console.WriteLine(palindrome.isPalindrome("racecar"));
+        // Console.WriteLine(palindrome.isPalindrome("hello"));
+        // Console.WriteLine(palindrome.isPalindrome("madam"));
 
         //checking the word frequency function
-        Dictionary<string, int> frequency = Word.GetWordFrequency();
-        foreach (KeyValuePair<string, int> pair in frequency)
-        {
-            Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
-        }
+        // Dictionary<string, int> frequency = Word.GetWordFrequency();
+        // foreach (KeyValuePair<string, int> pair in frequency)
+        // {
+        //     Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+        // }
 
 
-    Student.StudentGrade();
-    
+    // checking the student grade function
+    // Student.StudentGrade();
+
+    // checking the shapes function
+     Circle circle = new Circle { Name = "Circle", Radius = 5 };
+    Rectangle rectangle = new Rectangle { Name = "Rectangle", Width = 10, Height = 5 };
+    Triangle triangle = new Triangle { Name = "Triangle", Base = 8, Height = 6 };
+
+    PrintShapeArea(circle);
+    PrintShapeArea(rectangle);
+    PrintShapeArea(triangle);
     }
 
-    //checking the student grade function
+    static void PrintShapeArea(Shape shape) {
+        Console.WriteLine("{0} area: {1}", shape.Name, shape.Area());
+    }
+
 
 
 }
