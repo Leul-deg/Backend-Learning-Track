a6a624533de430e4a4b37ebf433bcdca2b7dacc1
Leul Degarege <leuldegarege11@gmail.com>
2023-08-07 15:16:57 +0300

    student management

--- student_management_system/Program.cs
+++ student_management_system/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main(string[] args) {
+        StudentList<Student> studentList = new StudentList<Student>();
+        studentList.AddStudent(new Student { Name = "Alice", Age = 20 });
+        studentList.AddStudent(new Student { Name = "Bob", Age = 21 });
+        studentList.AddStudent(new Student { Name = "Charlie", Age = 22 });
+
+        string filePath = "students.json";
+        studentList.SaveToJson(filePath);
+
+        StudentList<Student> loadedStudentList = new StudentList<Student>();
+        loadedStudentList.LoadFromJson(filePath);
+
+        Console.WriteLine("Loaded students:");
+        foreach (Student student in loadedStudentList.SearchByName("Alice")) {
+            Console.WriteLine($"{student.Name}, {student.Age}");
+        }
+    }
+}
--- student_management_system/student_list.cs
+++ student_management_system/student_list.cs
@@ -1,27 +1,35 @@
-class StudentList<T>{
-
-
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+class StudentList<T> where T : Student {
     private List<T> students = new List<T>();
 
-    public void AddStudent(T student){
+    public void AddStudent(T student) {
         students.Add(student);
     }
 
-    public void RemoveStudent(T student){
+    public void RemoveStudent(T student) {
         students.Remove(student);
     }
 
-    public void SearchByName(string name){
-        
-        var student = from student in students
-        where student.name == name
-        select student;
-        
-        }
+    public List<T> SearchByName(string name) {
+        var result = from student in students
+                     where student.Name == name
+                     select student;
+        return result.ToList();
+    }
 
+    public void SaveToJson(string filePath) {
+        string jsonString = JsonSerializer.Serialize(students);
+        File.WriteAllText(filePath, jsonString);
+    }
 
-
-        // private int count;
-
-
+    public void LoadFromJson(string filePath) {
+        string jsonString = File.ReadAllText(filePath);
+        students = JsonSerializer.Deserialize<List<T>>(jsonString);
+    }
 }
+
+
